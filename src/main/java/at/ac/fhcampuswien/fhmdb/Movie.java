8c7c7ae39439052c3875b32495a098e3342c80e8
LAPTOP-4HEA5EIP\nadin <nadine.kirnbauer@stud.fh-campuswien.ac.at>
2023-03-12 19:19:57 +0100

    navin1

--- src/main/java/at/ac/fhcampuswien/fhmdb/Movie.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/Movie.java
@@ -7,56 +7,38 @@ import java.util.List;
 public class Movie {
     private String title;
     private String description;
-    private List<Genre> genres;
-    private static List<Movie> movies = new ArrayList<>();
+    private List<Genre> genres = new ArrayList<>();
+
 
     public Movie(String title) {
         this.title = title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public void setDescription(String description) {
         this.description = description;
     }
-
+    public void addGenre(Genre genre) { this.genres.add(genre); }
     public void setGenres(List<Genre> genres) {
         this.genres = genres;
     }
-
     public String getTitle() {
         return title;
     }
-
     public String getDescription() {
         return description;
     }
-
     public List<Genre> getGenres() {
         return genres;
     }
-
     public String getGenresInStringFormat() {
         return genres.toString();
     }
 
-    public static Movie getMovieByTitle(String title) {
-        for (Movie movie : movies) {
-            if (movie.getTitle().equals(title)) {
-                return movie;
-            }
-        }
-        return null;
-
-    }
-
-    public static List<Movie> getMovies() {
-        return movies;
-    }
 
     public static List<Movie> initializeMovies() {
+        List<Movie> movies = new ArrayList<>();
 
         Movie movie = new Movie("Campus Couple Love Story");
         movie.setDescription(
@@ -72,13 +54,13 @@ public class Movie {
 
         movie = new Movie("Operating Systems o´clock");
         movie.setDescription(
-                "It is time for operating systems. Get the laptops out, start the console line and let´s go. Every command will effect something different, every combination\n" + "an individual masterpiece. It is up to the programmer, what he wants to do next. Will all the missing parts be found to succeed? Will all riddles get solved?\n" + "This is an up close movie to the fight of getting all points in the operating system test on Oracle.\n");
+                "It is time for operating systems. Get the laptops out, start the console line and let´s go. Every command will effect something different, every combination\n" +"an individual masterpiece. It is up to the programmer, what he wants to do next. Will all the missing parts be found to succeed? Will all riddles get solved?\n" + "This is an up close movie to the fight of getting all points in the operating system test on Oracle.\n");
         movie.setGenres(Arrays.asList(Genre.ACTION, Genre.ANIMATION, Genre.SCIENCE_FICTION));
         movies.add(movie);
 
         movie = new Movie("Operating Systems o´clock 2");
         movie.setDescription(
-                "They failed. But only so closely. But nevertheless they failed. What do to now? Preparing better, getting better, being better! But will it be enough in the\n" + "end? Or will they have to submit themselves to the powers of the complexity of the operating systems ...\n");
+                "They failed. But only so closely. But nevertheless they failed. What do to now? Preparing better, getting better, being better! But will it be enough in the\n" +"end? Or will they have to submit themselves to the powers of the complexity of the operating systems ...\n");
         movie.setGenres(Arrays.asList(Genre.DRAMA, Genre.ACTION, Genre.ANIMATION, Genre.SCIENCE_FICTION));
         movies.add(movie);
 
@@ -99,20 +81,17 @@ public class Movie {
         movie.setGenres(Arrays.asList(Genre.MUSICAL, Genre.FAMILY, Genre.ADVENTURE));
         movies.add(movie);
 
-        movie = new Movie("Rubber ducks invasion");
+        movie = new Movie("Rubber duck invasion");
         movie.setDescription(
                 "The first rubber duck was found nowhere less special than in David Brek's backpack on his way to a heist. He, most feared leader, and his accomplices\n" + "were the most-wanted criminals in whole Western City. Since that moment, even more and more ducks appeared in their backpacks. No matter how\n" + "much they observed their backpacks, the duck drama didn´t end. If they don´t stop the duck avalanche in time they will be found. The time is ticking.\n");
         movie.setGenres(Arrays.asList(Genre.MYSTERY, Genre.WESTERN, Genre.FAMILY, Genre.COMEDY, Genre.ADVENTURE));
         movies.add(movie);
 
-<<<<<<<< HEAD:src/main/java/at/ac/fhcampuswien/fhmdb/fhmdb/Movie.java
-========
         movie = new Movie("Howdy Campus");
         movie.setDescription(
                 "Explore the Wild West in 'Howdy Campus: Uncovering the Origins of the FH'.\nWitness the struggles of a small group of pioneers wanting to bring education in a land without laws and rules.\nWill they be successful? This captivating documentary is a must-see for anyone who loves history, the Wild West, and the power of knowledge.\"");
         movie.setGenres(Arrays.asList(Genre.DOCUMENTARY, Genre.WESTERN, Genre.HISTORY));
 
->>>>>>>> 728af08 (first test running):src/main/java/at/ac/fhcampuswien/fhmdb/Movie.java
         movie = new Movie("Finishing the race");
         movie.setDescription(
                 "The finish line in vision, only view last miles left. After running for so long the race between students and test are finally to an end. Now the have to face\n" + "their last ultimate enemy. They have to find out if they are up to this task after such long fighting or if they just got exhausted by now? If they are able to\n" + "overtake, what next? What is there after the finishing line, is it even worth it or will there be just another running track till infinity ...\n");
@@ -123,37 +102,4 @@ public class Movie {
         return movies;
     }
 
-    public static List<Movie> filterAfterGenre(Genre genre) {
-        if (genre == Genre.ALL) {
-            return movies;
-        } else {
-            List<Movie> filteredMovies = new ArrayList<>();
-            for (Movie movie : movies) {
-                if (movie.getGenres().contains(genre)) {
-                    filteredMovies.add(movie);
-                }
-            }
-            return filteredMovies;
-        }
-    }
-    public static List<Movie> searchingMovies(String searchedWord) {
-        searchedWord = makeStringUniform(searchedWord);
-        if (searchedWord.isEmpty()) {
-            return movies;
-        } else {
-            List<Movie> searchedMovies = new ArrayList<>();
-            for (Movie movie : movies) {
-                if (makeStringUniform(movie.description).contains(searchedWord) ||
-                        makeStringUniform(movie.title).contains(searchedWord) ||
-                        makeStringUniform(movie.genres.toString()).contains(searchedWord)) {
-                    searchedMovies.add(movie);
-                }
-            }
-
-            return searchedMovies;
-        }
-    }
-    public static String makeStringUniform(String polyformString){
-        return polyformString.toLowerCase().trim().replaceAll("\\s", "");
-    }
 }
