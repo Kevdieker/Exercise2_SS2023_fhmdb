453694b090da975ec9d600c0f9294c9f934c1c31
fatima204 <fatima.amrin.hossain@gmail.com>
2023-04-06 22:07:56 +0200

    MovieAPI Class added

---
+++ src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
@@ -0,0 +1,12 @@
+package at.ac.fhcampuswien.fhmdb;
+
+public class MovieAPI{
+
+    private final String URL = "https://prog2.fh-campuswien.ac.at/movies";
+    private final String DELIMITER = "&";
+
+    private String buildURL(){
+        return URL;
+    }
+
+}
