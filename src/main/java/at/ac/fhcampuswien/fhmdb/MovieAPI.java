a2367dc53ab3010a82a439e90d868a1faf3b2ec6
fatima204 <fatima.amrin.hossain@gmail.com>
2023-04-07 23:33:12 +0200

    OkHttpClient correctly added

--- src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/MovieAPI.java
@@ -1,29 +1,32 @@
 package at.ac.fhcampuswien.fhmdb;
 
+import okhttp3.*;
 public class MovieAPI{
 
     private final String URL = "https://prog2.fh-campuswien.ac.at/movies";
 
-    private String urlBuilder(String query, Genre genre, String releaseYear, String ratingFrom){
+    private String urlBuilder(String query, Genre genre, String releaseYear, String ratingFrom) {
 
         StringBuilder url = new StringBuilder(URL);
 
-        if((!query.isEmpty() && query != null)|| genre != null && releaseYear != null && ratingFrom != null){
+        if ((!query.isEmpty() && query != null) || genre != null && releaseYear != null && ratingFrom != null) {
             url.append("?");
         }
-        if(!query.isEmpty() && query != null){
+        if (!query.isEmpty() && query != null) {
             url.append("query=").append(query).append("&");
         }
-        if(genre != null){
+        if (genre != null) {
             url.append("genre=").append(genre).append("&");
         }
-        if(releaseYear != null){
+        if (releaseYear != null) {
             url.append("releaseYear=").append(releaseYear).append("&");
         }
-        if(ratingFrom != null){
+        if (ratingFrom != null) {
             url.append("ratingFrom=").append(ratingFrom);
         }
         return url.toString();
     }
+
+    OkHttpClient client = new OkHttpClient();
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module at.ac.fhcampuswien.fhmdb.fhmdb {
     requires javafx.controls;
     requires javafx.fxml;
+    requires okhttp3;
 
 
     opens at.ac.fhcampuswien.fhmdb to javafx.fxml;
