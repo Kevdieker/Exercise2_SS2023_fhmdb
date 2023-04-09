9f3747299fc1058b5bc4173cabc2b3a08c5d797d
Kevker <kevin.kerbl.kk@gmail.com>
2023-04-09 21:36:10 +0200

    added dependencies + tests + streams + Api + major changes to setter getter and adder

--- src/main/java/at/ac/fhcampuswien/fhmdb/Home.java
+++ src/main/java/at/ac/fhcampuswien/fhmdb/Home.java
@@ -12,6 +12,7 @@ import static at.ac.fhcampuswien.fhmdb.Movie.*;
 
 public class Home extends Application {
 
+
     @Override
     public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("home.fxml")));
